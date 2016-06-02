10e8b022c0220086f2279f64413e87da8599d95b
Michael Triantafelow <MTriantafelow@gmail.com>
2016-06-01 21:59:31 -0400

    final solution

---
+++ test/GameTest.js
@@ -0,0 +1,32 @@
+describe("Game", () => {
+    let game;
+    
+    beforeEach(() => {
+        game = Game.make();
+    });
+    
+    it("can score a gutter game", () => {
+        game.roll([0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0]);
+        expect(game.score()).toBe(0);
+    });
+    
+    it("can score a game of ones", () => {
+        game.roll([1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1]);
+        expect(game.score()).toBe(20);
+    });
+    
+    it("can score a spare", () => {
+        game.roll([1,9, 3,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0]);
+        expect(game.score()).toBe(16);
+    });
+    
+    it("can score a strike", () => {
+        game.roll([10, 3,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0]);
+        expect(game.score()).toBe(22);
+    });
+    
+    it("can score a perfect game", () => {
+        game.roll([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]);
+        expect(game.score()).toBe(300);
+    });
+});
